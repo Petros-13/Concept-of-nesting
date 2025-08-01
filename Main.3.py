86c91931aa375cbac343e4e56f2b69157940464f
Petros-13 <petroskalkounos78@gmail.com>
2025-08-01 16:03:40 +0200

    3rd commit

---
+++ Main.3.py
@@ -0,0 +1,13 @@
+ride=(input("Enter your ride"))
+if ride=="motorbike":
+    motorbike_type=(input("Enter your perffered type"))
+    if motorbike_type=="Kawasaki":
+        print("your ride is ready")
+    else:
+        print("Try another ride that is not available")
+else:
+    car_type=(input("Enter yor perffered car type"))
+    if car_type=="BMW":
+        print("Your ride is ready")
+    else:
+        print("This car is unavailable")
